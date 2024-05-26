00f0259eaf4ce55fdd6b8f378b4707e5c9970cb1
Martin Nebehay <martinnebe456@gmail.com>
2024-05-26 16:44:59 +0200

    External API ARES

--- src/main/java/cz/uun/companydatabase/service/CompanyService.java
+++ src/main/java/cz/uun/companydatabase/service/CompanyService.java
@@ -2,9 +2,11 @@ package cz.uun.companydatabase.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import cz.uun.companydatabase.dtoin.CompanyCreateDtoIn;
 import cz.uun.companydatabase.dtoin.CompanyUpdateDtoIn;
+import cz.uun.companydatabase.dtoin.AresResponseDto;
 import cz.uun.companydatabase.entity.Company;
 import cz.uun.companydatabase.repository.CompanyRepository;
 
@@ -15,6 +17,9 @@ public class CompanyService {
     
     @Autowired
     private CompanyRepository companyRepository;
+
+    @Autowired
+    private RestTemplate restTemplate;
 
     public Company createCompany(CompanyCreateDtoIn dtoIn) {
         Company company = new Company();
@@ -52,4 +57,9 @@ public class CompanyService {
         }
         return null;
     }
+    
+    public Company getCompanyByIcoFromAres(String url){
+        AresResponseDto aresResponse = restTemplate.getForObject(url, AresResponseDto.class);
+        return null;
+    } 
 }
