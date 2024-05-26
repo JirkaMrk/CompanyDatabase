3a5d372f2edd6816358f55fe12bc35a1f05810b0
JirkaMrk <125826485+JirkaMrk@users.noreply.github.com>
2024-05-26 10:12:30 +0200

    Initial

---
+++ src/main/java/cz/uun/companydatabase/CompanyDatabaseApplication.java
@@ -0,0 +1,13 @@
+package cz.uun.companydatabase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompanyDatabaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompanyDatabaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cz/uun/companydatabase/CompanyDatabaseApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.uun.companydatabase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompanyDatabaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
