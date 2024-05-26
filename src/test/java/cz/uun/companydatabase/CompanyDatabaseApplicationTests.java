f56f1e6b9f701e8ebf33ed8324a135f3c970cbcc
JirkaMrk <125826485+JirkaMrk@users.noreply.github.com>
2024-05-26 18:45:07 +0200

    Todo
    -right order of tests
    -delete response

--- src/test/java/cz/uun/companydatabase/CompanyDatabaseApplicationTests.java
+++ src/test/java/cz/uun/companydatabase/CompanyDatabaseApplicationTests.java
@@ -1,13 +1,69 @@
 package cz.uun.companydatabase;
 
-import org.junit.jupiter.api.Test;
+import cz.uun.companydatabase.dtoin.CompanyCreateDtoIn;
+import cz.uun.companydatabase.entity.Company;
+import org.junit.Test;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.test.context.junit4.SpringRunner;
 
-@SpringBootTest
-class CompanyDatabaseApplicationTests {
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+
+public class CompanyDatabaseApplicationTests {
+
+	@Autowired
+	private TestRestTemplate restTemplate;
+
+	//TODO order
 
 	@Test
-	void contextLoads() {
+	@Order(1)
+	public void testCreateCompany() {
+		CompanyCreateDtoIn dtoIn = new CompanyCreateDtoIn();
+		dtoIn.setAddress("ulr");
+		dtoIn.setIco("9999");
+		dtoIn.setName("dd");
+
+		Company body = this.restTemplate.postForObject("/company", dtoIn, Company.class);
+		assertThat(body.getId()).isNotZero();
+		assertThat(body.getAddress()).isEqualTo("ulr");
+		assertThat(body.getIco()).isEqualTo("9999");
+		assertThat(body.getName()).isEqualTo("dd");
+	}
+
+	@Test
+	@Order(2)
+	public void testGetCompanyById() {
+		Company body = this.restTemplate.getForObject("/company/9999", Company.class);
+		assertThat(body.getIco()).isEqualTo("9999");
+	}
+
+	@Test
+	@Order(3)
+	public void testUpdateCompanyByID() {
+		CompanyCreateDtoIn dtoIn = new CompanyCreateDtoIn();
+		dtoIn.setName("xx");
+
+		this.restTemplate.put("/company/9999", dtoIn);
+
+		Company updatedCompany = this.restTemplate.getForObject("/company/9999", Company.class);
+		assertThat(updatedCompany.getName()).isEqualTo("xx");
+	}
+
+	@Test
+	@Order(4)
+	public void testDeleteCompanyById() {
+		this.restTemplate.delete("/company/9999");
+	//TODO	assertThat(get()).isEqualTo(HttpStatus.NOT_FOUND);
 	}
 
 }
